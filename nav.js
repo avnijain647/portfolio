99ec7e77a7dac368ea8a4947b6a82674be8a2d6b
avnijain647 <47116281+avnijain647@users.noreply.github.com>
2023-04-06 21:18:52 +0530

    Add files via upload

---
+++ nav.js
@@ -0,0 +1,140 @@
+
+
+$(function() {
+    var img = $('.rick').html();
+    for (var i = 0; i < 10; i++) {
+        moar(img);
+    }
+    $(window).on('scroll', function() {
+        if ($(this).scrollTop() >= $('.rick').height() - $(this).height()) {
+            moar(img);
+        }
+    });
+    function moar(content) {
+        $('.rick').append(content);
+    }
+});
+
+$('.script3').hover(function(){
+   $('.script6_1').fadeToggle(0)
+   $('.script8').fadeToggle(0)
+   $('.script5').fadeToggle(0)
+   $('.script9').toggleClass('inline')
+})
+$('.script5').hover( function(){
+ $('.script8').fadeToggle(0)
+   $('.script10').toggleClass('inline')
+})
+
+$(".loading, .loadingbox").show()
+
+setTimeout(function () {
+  $(".loading, .loadingbox").fadeOut(1000)
+}, 2000)
+
+//NooraHealth
+
+var animation = bodymovin.loadAnimation({
+container: document.getElementById('nh-container0'),
+path: 'fig-cover-square.json',
+renderer: 'svg',
+loop: true,
+autoplay: true,
+name: "Demo Animation",
+});
+
+var animation = bodymovin.loadAnimation({
+container: document.getElementById('nh-container1'),
+path: 'WorldMentalHealthDayPost.json',
+renderer: 'svg',
+loop: true,
+autoplay: true,
+name: "Demo Animation",
+});
+
+var animation = bodymovin.loadAnimation({
+container: document.getElementById('nh-container2'),
+path: 'WorldPatientDayPost.json',
+renderer: 'svg',
+loop: true,
+autoplay: true,
+name: "Demo Animation",
+});
+
+
+var animation = bodymovin.loadAnimation({
+container: document.getElementById('nh-container3'),
+path: 'WorldHeartDayPost.json',
+renderer: 'svg',
+loop: true,
+autoplay: true,
+name: "Demo Animation",
+});
+
+
+
+var animation = bodymovin.loadAnimation({
+container: document.getElementById('nh-container5'),
+path: 'Icon-gifs.json',
+renderer: 'svg',
+loop: true,
+autoplay: true,
+name: "Demo Animation",
+});
+
+var animation = bodymovin.loadAnimation({
+container: document.getElementById('nh-container6'),
+path: 'nh-animal-gif.json',
+renderer: 'svg',
+loop: true,
+autoplay: true,
+name: "Demo Animation",
+});
+
+var animation = bodymovin.loadAnimation({
+container: document.getElementById('nh-container7'),
+path: 'nh-brand-applications.json',
+renderer: 'svg',
+loop: true,
+autoplay: true,
+name: "Demo Animation",
+});
+
+//Fig
+var animation = bodymovin.loadAnimation({
+container: document.getElementById('fig-container1'),
+path: 'fig-time-sm.json',
+renderer: 'svg',
+loop: true,
+autoplay: true,
+name: "Demo Animation",
+});
+
+
+
+var animation = bodymovin.loadAnimation({
+container: document.getElementById('fig-container2'),
+path: 'fig-sm-mockup-1.json',
+renderer: 'svg',
+loop: true,
+autoplay: true,
+name: "Demo Animation",
+});
+
+var animation = bodymovin.loadAnimation({
+container: document.getElementById('fig-container3'),
+path: 'fig-sm-mockup-2.json',
+renderer: 'svg',
+loop: true,
+autoplay: true,
+name: "Demo Animation",
+});
+
+var animation = bodymovin.loadAnimation({
+container: document.getElementById('fig-container4'),
+path: 'fig-thematic.json',
+renderer: 'svg',
+loop: true,
+autoplay: true,
+name: "Demo Animation",
+});
