f683e731f97b23c679d01d74dceba5778fb3afa1
avnijain647 <javni647@gmail.com>
2023-06-11 16:53:37 +0530

    small adjustments css js

--- nav.js
+++ nav.js
@@ -1,17 +1,17 @@
 
 
 $(function() {
-    var img = $('.rick').html();
+    var img = $('.rick2').html();
     for (var i = 0; i < 10; i++) {
         moar(img);
     }
     $(window).on('scroll', function() {
-        if ($(this).scrollTop() >= $('.rick').height() - $(this).height()) {
+        if ($(this).scrollTop() >= $('.rick2').height() - $(this).height()) {
             moar(img);
         }
     });
     function moar(content) {
-        $('.rick').append(content);
+        $('.rick2').append(content);
     }
 });
 
