ed60dbeb3be1deedd76c56c112e220e0f978efa3
avnijain647 <javni647@gmail.com>
2024-02-18 22:21:48 +0530

    tff

--- nav.js
+++ nav.js
@@ -15,6 +15,23 @@ $(function() {
     }
 });
 
+
+
+$(function() {
+    var img = $('.rick').html();
+    for (var i = 0; i < 10; i++) {
+        moar(img);
+    }
+    $(window).on('scroll', function() {
+        if ($(this).scrollTop() >= $('.rick').height() - $(this).height()) {
+            moar(img);
+        }
+    });
+    function moar(content) {
+        $('.rick').append(content);
+    }
+});
+
 $('.script3').hover(function(){
    $('.script6_1').fadeToggle(0)
    $('.script8').fadeToggle(0)
