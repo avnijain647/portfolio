9b2375e5b4387ea46e2bc6300f031b53d6159f49
avnijain647 <javni647@gmail.com>
2025-04-08 23:17:28 +0530

    removed infinite scroll

--- nav.js
+++ nav.js
@@ -1,19 +1,19 @@
 //
 //
-$(function() {
-    var img = $('.rick2').html();
-    for (var i = 0; i < 10; i++) {
-        moar(img);
-    }
-    $(window).on('scroll', function() {
-        if ($(this).scrollTop() >= $('.rick2').height() - $(this).height()) {
-            moar(img);
-        }
-    });
-    function moar(content) {
-        $('.rick2').append(content);
-    }
-});
+// $(function() {
+//     var img = $('.rick2').html();
+//     for (var i = 0; i < 10; i++) {
+//         moar(img);
+//     }
+//     $(window).on('scroll', function() {
+//         if ($(this).scrollTop() >= $('.rick2').height() - $(this).height()) {
+//             moar(img);
+//         }
+//     });
+//     function moar(content) {
+//         $('.rick2').append(content);
+//     }
+// });
 //
 // $(function() {
 //     var img = $('.rick').html();
